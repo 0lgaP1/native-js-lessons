3f3ba1cbd9cfdeaf6f93cb1d8323fb10c524fe6f
0lgaP1 <volhapatapchuk@gmail.com>
2024-07-27 23:23:28 +0200

    Reduce part 2

--- lesson3_homework.js
+++ lesson3_homework.js
@@ -71,21 +71,65 @@ function sumNestedArrays (nestedArrays) {
 }
 console.log(sumNestedArrays([[1, 2], [3, 4, 5], [6, 7, 8, 9],])) //Output: 45
 
-//
+// console.log if the quantity of same numbers is 1
 
 const numbers = [1, 2, 3, 3, 4, 5, 5, 6];
-console.log(removeDuplicates(numbers)); // Выведет: [1, 2, 3, 4, 5, 6]
-
-//
+function removeDuplicates (numbers) {
+    return numbers.reduce((unique, num) => { 
+        return unique.includes(item) ? unique : [...unique, item]}, [])};
+// Выведет: [1, 2, 3, 4, 5, 6] 
+// также можно воспользоваться объектом Set:
+// function removeDuplicates2(numbers) {
+//     return [...new Set(numbers)];
+// }
+console.log(removeDuplicates(numbers)); 
+// find duplicates and return them
 
 const array = [1, 2, 3, 2, 4, 5, 4, 5];
 const duplicatesArray = findDuplicates(array);
+function findDuplicates (array) {
+const seen = [];
+    return array.reduce((duplicates, num) => {
+        if (seen.includes(num)) {
+            if (!duplicates.includes(num)) {
+                duplicates.push(num);
+            }
+        } else {
+            seen.push(num);
+        }
+        return duplicates;
+    }, [])};
 console.log(duplicatesArray); // Output: [2, 4, 5]
 
-//
+//or option2 for the task above 
+// function findDuplicates(array) {
+//     const seen = new Set();
+//     return array.reduce((duplicates, num) => {
+//         if (seen.has(num)) {
+//             if (!duplicates.includes(num)) {
+//                 duplicates.push(num);
+//             }
+//         } else {
+//             seen.add(num);
+//         }
+//         return duplicates;
+//     }, []);
+// }
+
+// the result should be an object that returns unique strings as keys and its' quantity as values
 
 const words = ['apple', 'banana', 'apple', 'cherry', 'banana', 'apple'];
-
+function wordCount (words) {
+    return words.reduce((acc, item) => {
+        if (acc[item]) {
+            acc[item]++;
+        } else {
+            acc[item] = 1;
+        }
+        return acc,
+    }, {});
+}
+   
 console.log(wordCount); // Output: { apple: 3, banana: 2, cherry: 1 }
 
 //
@@ -96,6 +140,12 @@ const people = [
     { name: 'Charlie', age: 22 },
    ];
    
+   function getPropertyValues(arr, property) {
+    return arr.reduce((result, obj) => {
+      result.push(obj[property]);
+      return result;
+    }, []);
+   }
    const names = getPropertyValues(people, 'name');
    console.log(names); // Output: ['Alice', 'Bob', 'Charlie']
    
